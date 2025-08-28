08738b7a8a3118d2e86283df01d1a26775fd707d
Muskan Soni <iammuskansonii@gmail.com>
2025-08-28 22:53:04 +0530

    String compression

---
+++ Strings/StringCompression.java
@@ -0,0 +1,35 @@
+package Strings;
+public public class StringCompression{
+    public int compress(char[] chars) {
+        int write = 0;
+        int read = 0;
+
+        while (read < chars.length) {
+            char current = chars[read];
+            int count = 0;
+            while (read < chars.length && chars[read] == current) {
+                read++;
+                count++;
+            }
+            chars[write++] = current;
+            if (count > 1) {
+                for (char c : String.valueOf(count).toCharArray()) {
+                    chars[write++] = c;
+                }
+            }
+        }
+        return write;
+    }
+
+    public static void main(String[] args) {
+        Solution StringCompression = new StringCompression();
+        char[] chars = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+        int newLength = solution.compress(chars);
+        System.out.println("Compressed Length: " + newLength);
+        System.out.print("Compressed Array: ");
+        for (int i = 0; i < newLength; i++) {
+            System.out.print(chars[i] + " ");
+        }
+        System.out.println();
+    }
+} 
