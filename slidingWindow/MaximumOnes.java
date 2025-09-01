63fd3fcfc9312b1f5e989bf9dd66a3d10ffa1b5e
Muskan Soni <iammuskansonii@gmail.com>
2025-09-01 10:41:08 +0530

    maximum ones i

---
+++ slidingWindow/MaximumOnes.java
@@ -0,0 +1,25 @@
+package slidingWindow;
+public class MaximumOnes {
+     public int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0;
+        int count = 0;
+        
+        for (int num : nums) {
+            if (num == 1) {
+                count++;
+                maxCount = Math.max(maxCount, count);
+            } else {
+                count = 0;
+            }
+        }
+        
+        return maxCount;
+    }
+    
+    public static void main(String[] args) {
+        MaximumOnes solution = new MaximumOnes();
+        int[] nums = {1, 1, 0, 1, 1, 1};
+        int result = solution.findMaxConsecutiveOnes(nums);
+        System.out.println("Maximum consecutive ones: " + result);
+    }
+}
