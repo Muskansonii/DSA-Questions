deb38a6f41d6aac239f67bd20b7d0763635fcde4
Muskan Soni <iammuskansonii@gmail.com>
2025-09-16 11:01:13 +0530

    minimum time to finish one task

---
+++ Arrays/minimumTimeToFinishOneTask.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+public class minimumTimeToFinishOneTask {
+    public int earliestTime(int[][] tasks) {
+        int min = Integer.MAX_VALUE;
+        for (int[] task : tasks) {
+            int start = task[0];
+            int duration = task[1];
+            min = Math.min(min, start + duration);
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        minimumTimeToFinishOneTask sol = new minimumTimeToFinishOneTask();
+
+        // Example 1
+        int[][] tasks1 = {{1, 6}, {2, 3}};
+        System.out.println("Earliest finish time (Example 1): " + sol.earliestTime(tasks1)); // Output: 5
+
+        // Example 2
+        int[][] tasks2 = {{100, 100}, {100, 100}, {100, 100}};
+        System.out.println("Earliest finish time (Example 2): " + sol.earliestTime(tasks2)); // Output: 200
+
+        // You can add more test cases here if you want!
+    }
+}
+    
+
