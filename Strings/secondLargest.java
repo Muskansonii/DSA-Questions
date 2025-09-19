d2e2ff29c01ac0df80ec7827593d994593417e24
Muskan Soni <iammuskansonii@gmail.com>
2025-09-19 14:21:37 +0530

    Second largest in a string

---
+++ Strings/secondLargest.java
@@ -0,0 +1,28 @@
+package Strings;
+class Solution {
+    public static int secondLargest(String s) {
+        int largest = -1;
+        int secondLargest = -1;
+
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                int d = Character.getNumericValue(ch);
+
+                if (d > largest) {
+                    secondLargest = largest;
+                    largest = d;
+                } else if (d > secondLargest && d < largest) {
+                    secondLargest = d;
+                }
+            }
+        }
+        return secondLargest; 
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        String input = "abc12345"; // You can change this input
+        int result = secondHighest(input);
+        System.out.println("The second highest digit is: " + result);
+    }
+}
